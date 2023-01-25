42cfaa9e0f8475625fa122721c761c951b4f53af
Marco Behler <marco@marcobehler.com>
2023-01-25 17:04:25 +0100

    Episode 3

--- src/main/java/com/jetbrains/marco/App.java
+++ src/main/java/com/jetbrains/marco/App.java
@@ -1,10 +1,16 @@
 package com.jetbrains.marco;
 
+import io.github.rctcwyvrn.blake3.Blake3;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
@@ -17,7 +23,9 @@ public class App {
     static String userHome = System.getProperty("user.home");
     static Path thumbnailsDir = Path.of(userHome).resolve(".photos");
 
-    public static void main(String[] args) throws IOException {
+    static ImageMagick magick = new ImageMagick();
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         Files.createDirectories(thumbnailsDir);
 
         String directory = args.length == 1 ? args[0] : ".";
@@ -26,17 +34,63 @@ public class App {
         AtomicInteger counter = new AtomicInteger();
         long start = System.currentTimeMillis();
 
-        try (Stream<Path> files = Files.walk(sourceDir)
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        try (Stream<Path> images = Files.walk(sourceDir)
                 .filter(Files::isRegularFile)
-                .filter(App::isImage)) {
-            files.forEach(f -> {
-                counter.incrementAndGet();
-                new ImageMagick().createThumbnail(f, thumbnailsDir.resolve(f.getFileName()));
-            });
+                .filter(App::isImage);
+        ) {
+            images.forEach(image -> executorService.submit(() -> {
+                final boolean success = createThumbnail(image);
+                if (success) counter.incrementAndGet();
+            }));
         }
+        executorService.shutdown();
+        executorService.awaitTermination(3, TimeUnit.HOURS);
 
         long end = System.currentTimeMillis();
         System.out.println("Converted " + counter + " images to thumbnails. Took " + ((end - start) * 0.001) + "seconds");
+    }
+
+    private static boolean createThumbnail(Path image) {
+        try {
+            Path thumbnail = getThumbnailPath(image);
+            return magick.createThumbnail(image, thumbnail);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static Path getThumbnailPath(Path image) throws IOException {
+        String hash = hash(image);
+        String dir = hash.substring(0, 2);
+        String filename = hash.substring(2);
+
+        Path storageDir = thumbnailsDir.resolve(dir);
+        if (!Files.exists(storageDir)) {
+            Files.createDirectories(storageDir);
+        }
+        return storageDir.resolve(filename + ".webp");
+    }
+
+    public static String hash(Path file) {
+        Blake3 hasher = Blake3.newInstance();
+
+        try (InputStream ios = Files.newInputStream(file)) {
+            byte[] buffer = new byte[8192];
+            int read;
+            while ((read = ios.read(buffer)) != -1) {
+                if (read == buffer.length) {
+                    hasher.update(buffer);
+                } else {
+                    hasher.update(Arrays.copyOfRange(buffer, 0, read));
+                }
+            }
+            return hasher.hexdigest();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "NA";
+        }
     }
 
     private static boolean isImage(Path path) {
@@ -51,7 +105,7 @@ public class App {
 
     public static class ImageMagick {
 
-        private Version version = detectVersion();
+        private final Version version = detectVersion();
 
         public int run(String... cmds) throws IOException, InterruptedException {
             ProcessBuilder builder = new ProcessBuilder(cmds);
@@ -78,11 +132,11 @@ public class App {
                 ProcessBuilder builder = new ProcessBuilder(cmd);
                 builder.inheritIO();
                 Process process = builder.start();
-                boolean finished = process.waitFor(3, TimeUnit.SECONDS);
+                boolean finished = process.waitFor(5, TimeUnit.SECONDS);
                 if (!finished) {
                     process.destroy();
                 }
-                return finished;
+                return finished & process.exitValue() == 0;
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
                 return false;
