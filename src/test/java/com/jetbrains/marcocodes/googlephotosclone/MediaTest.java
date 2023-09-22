2ed0b6d8b4c98e49836fbc2ec2ef28ba6ef032fe
Marco Behler <marco@marcobehler.com>
2023-09-22 12:31:26 +0200

    cleanup

--- src/test/java/com/jetbrains/marcocodes/googlephotosclone/MediaTest.java
+++ src/test/java/com/jetbrains/marcocodes/googlephotosclone/MediaTest.java
@@ -24,45 +24,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MediaTest {
 
     @TestFactory
-    Collection<DynamicTest> dynamicTestsCreated() {
-        List<String> result = new ArrayList<>();
-
+    Collection<DynamicTest> mediaTests() {
         try (ScanResult scanResult = new ClassGraph().scan()) {
-            scanResult
+            List<String> images = scanResult
                     .getResourcesMatchingPattern(Pattern.compile("image-(.*)\\.jpg"))
-                    .forEach((resource) -> {
-                        result.add(resource.getPath());
-                    });
+                    .getPaths();
+
+            return images.stream().map(image -> DynamicTest.dynamicTest(image, () -> {
+                String filename = image.substring(0, image.indexOf("."));
+
+                try (InputStream json = MediaTest.class.getResourceAsStream("/" + filename + ".json");
+                     InputStream imageStream = MediaTest.class.getResourceAsStream("/" + image)) {
+
+                    TestMetadata testMetadata = new ObjectMapper().registerModule(new JavaTimeModule()).readValue(json, TestMetadata.class);
+                    Metadata metadata = ImageMetadataReader.readMetadata(imageStream);
+
+                    Initializr.Dimensions dimensions = Initializr.getImageSize(metadata);
+                    assertThat(dimensions.height()).isEqualTo(testMetadata.height());
+                    assertThat(dimensions.width()).isEqualTo(testMetadata.width());
+
+                    LocalDateTime creationTime = Initializr.getCreationTime(Path.of(MediaTest.class.getResource("/" + image).getFile().substring(1)), metadata);
+                    assertThat(creationTime.truncatedTo(ChronoUnit.MINUTES)).isEqualToIgnoringSeconds(testMetadata.date());
+
+                    Location location = Initializr.getLocation(metadata);
+
+                    if (testMetadata.longitude() == null || testMetadata.latitude() == null) {
+                        assertThat(location).isNull();
+                    }
+                    else {
+                        assertThat(location.getDms()).isEqualTo(testMetadata.latitude() + ", " + testMetadata.longitude());
+                    }
+
+
+                }
+            })).collect(Collectors.toList());
         }
 
-        return result.stream().map(image -> DynamicTest.dynamicTest(image, () -> {
-            String filename = image.substring(0, image.indexOf("."));
 
-            try (InputStream json = MediaTest.class.getResourceAsStream("/" + filename + ".json");
-                 InputStream imageStream = MediaTest.class.getResourceAsStream("/" + image)) {
-
-                TestMetadata testMetadata = new ObjectMapper().registerModule(new JavaTimeModule()).readValue(json, TestMetadata.class);
-                Metadata metadata = ImageMetadataReader.readMetadata(imageStream);
-
-                Initializr.Dimensions dimensions = Initializr.getImageSize(metadata);
-                assertThat(dimensions.height()).isEqualTo(testMetadata.height());
-                assertThat(dimensions.width()).isEqualTo(testMetadata.width());
-
-                LocalDateTime creationTime = Initializr.getCreationTime(Path.of(MediaTest.class.getResource("/" + image).getFile().substring(1)), metadata);
-                assertThat(creationTime.truncatedTo(ChronoUnit.MINUTES)).isEqualToIgnoringSeconds(testMetadata.date());
-
-                Location location = Initializr.getLocation(metadata);
-
-                if (testMetadata.longitude() == null || testMetadata.latitude() == null) {
-                    assertThat(location).isNull();
-                }
-                else {
-                    assertThat(location.getDms()).isEqualTo(testMetadata.latitude() + ", " + testMetadata.longitude());
-                }
-
-
-            }
-        })).collect(Collectors.toList());
 
     }
 
