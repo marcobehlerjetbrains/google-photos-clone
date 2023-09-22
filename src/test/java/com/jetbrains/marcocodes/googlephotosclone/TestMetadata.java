9ca5eca9dacd329ec3a5761c6b78ff751b41c2ee
Marco Behler <marco@marcobehler.com>
2023-09-22 16:31:05 +0200

    cleanup

--- src/test/java/com/jetbrains/marcocodes/googlephotosclone/TestMetadata.java
+++ src/test/java/com/jetbrains/marcocodes/googlephotosclone/TestMetadata.java
@@ -6,5 +6,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.LocalDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
-public record TestMetadata(Integer width, Integer height, LocalDateTime date, String latitude, String longitude) {
+public record TestMetadata(Integer width,
+                           Integer height,
+                           LocalDateTime date,
+                           String latitude,
+                           String longitude) {
 }
