31e8e7a7b0513802f005f8bf6e996af273b42d25
Marco Behler <marco.behler@jetbrains.com>
2023-10-23 20:07:54 +0200

    working on fullscan

--- src/test/java/com/jetbrains/marcocodes/googlephotosclone/MediaScannerTest.java
+++ src/test/java/com/jetbrains/marcocodes/googlephotosclone/MediaScannerTest.java
@@ -8,6 +8,20 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+/**
+ *
+ *
+ gradle test --tests org.gradle.SomeTest.someSpecificFeature
+ gradle test --tests '*SomeTest.someSpecificFeature'
+ gradle test --tests '*SomeSpecificTest'
+ gradle test --tests 'all.in.specific.package*'
+ gradle test --tests '*IntegTest'
+ gradle test --tests '*IntegTest*ui*'
+ gradle test --tests '*IntegTest.singleMethod'
+ gradle someTestTask --tests '*UiTest' someOtherTestTask --tests '*WebTest*ui'
+ *
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class MediaScannerTest {
 
@@ -16,7 +30,10 @@ class MediaScannerTest {
 
     @Test
     void fullscan() {
+
         mediaScanner.fullscan(Path.of("c:\\tmp"));
+
+        // print out time
     }
 
 
